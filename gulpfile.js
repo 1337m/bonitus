347fb082f3907ae3b708291d760431e9c7e2e032
Bartosz Bąk <barbak1994@gmail.com>
2017-02-08 23:35:09 +0100

    Creating compiler

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+let babel = require('gulp-babel');
+let eslint = require('gulp-eslint');
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let scsslint = require('gulp-sass-lint');
+
+gulp.task('default', [
+    'sass:lint',
+    'es6:lint',
+    'sass',
+    'es6',
+    'sass:watch',
+    'es6:watch'
+]);
+
+gulp.task('es6', () =>{
+    return gulp
+        .src('./src/app.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('es6:lint', () => {
+    return gulp
+        .src(['./src/**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('es6:watch', () => {
+    return gulp
+        .watch('./src/**/*.js', ['es6:lint', 'es6']);
+});
+
+gulp.task('sass', () => {
+    return gulp
+        .src('./src/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('sass:lint', () => {
+    return gulp
+        .src('./src/**/*.scss')
+        .pipe(scsslint());
+});
+
+gulp.task('sass:watch', () => {
+    return gulp
+        .watch('./src/**/*.scss', ['sass:lint', 'sass']);
+});
